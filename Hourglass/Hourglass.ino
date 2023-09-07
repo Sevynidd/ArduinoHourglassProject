a2c4d2930f151cddf3ad02c3069157104200df77
itsjustahobbyman <142495564+itsjustahobbyman@users.noreply.github.com>
2023-09-07 18:36:24 +0200

    funktionierender seitenwechsel

--- Hourglass/Hourglass.ino
+++ Hourglass/Hourglass.ino
@@ -10,6 +10,7 @@
 
 int mapMatrix1[8][8];
 int mapMatrix2[8][8];
+bool hilfsmatrix[8][8];
 
 //Gyro
 #include <Wire.h>
@@ -45,7 +46,7 @@ void setup() {
   delay(100);
   mpu.calcGyroOffsets();
   Serial.println("Done.");
-
+ 
 
   // setzen der unteren 60 LEDs (weil 60sek/min)
   for (int i = 8; i <= 15; i++) {    
@@ -56,52 +57,34 @@ void setup() {
   matrix.setPoint(7, 9, false);
   matrix.setPoint(6, 9, false);
 }
-void swapUpwards(){
-    for(int i=0; i<8; i++){
-      for(int j=0; j<8; j++){
-        if(matrix.getPoint(i,j)){
 
-            matrix.setPoint(7-i,7-j, true);
-            matrix.setPoint(i,j, false);
-          
-        }
-      }
-    }
-}
-
-void swapDownwards(){
-    for(int i=7; i>=0; i--){
-      for(int j=7; j>=0; j--){
-        if(matrix.getPoint(i,j)){
-            matrix.setPoint(7-i,7-j, true);
-            matrix.setPoint(i,j, false);
-
-          }
-        }
-      }
-    }
   
-
 
   bool kopf = true;
   int currX = 0; //X Koordinate die beschrieben werden soll
   int currY = 0; //Y Koordinate die beschrieben werden soll
   int state = 0;
+
+     
 void loop() {
   mpu.update();
-    // swapDownwards();
-    // delay(1000);
 
-    // swapUpwards();
-    // delay(1000);
+  Serial.print("X: " + String(mpu.getAngleX()));
+  Serial.println("\tY: " + String(mpu.getAngleY()));
   if (state == 60 & kopf){
 
   }
   else{
-  Serial.print("X: " + String(mpu.getAngleX()));
-  Serial.println("\tY: " + String(mpu.getAngleY()));
+swap();
+delay(100);
+swap();
+delay(100);
+
+  //Wert größer als die Möglichkeiten setzen 
   currX = 8;
   currY = 8;
+  
+  //niedrigsten freien Punkt suchen
   for(int i=1; i<=8; i++){
     for(int j=1; j<=8; j++){
       if(matrix.getPoint(i-1, j-1)==false){
@@ -128,3 +111,22 @@ void loop() {
 }
 
 
+void swap(){
+  
+  for(int i=0;i<8;i++){
+    for(int j=0;j<8;j++){
+      if(matrix.getPoint(i,j)){
+        hilfsmatrix[7-i][7-j]= true;
+      }
+      else{
+        hilfsmatrix[7-i][7-j]= false;
+      }
+    }
+  }
+  for(int i=0;i<8;i++){
+    for(int j=0;j<8;j++){
+      matrix.setPoint(i,j, hilfsmatrix[i][j]);
+    }
+  }
+
+}
