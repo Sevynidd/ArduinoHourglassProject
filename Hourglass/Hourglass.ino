aef818a6b16b8e0fb528f44721da1290977ea4cb
Sevynidd <Sevynidd02@protonmail.com>
2023-09-08 17:06:38 +0200

    Code formatting

--- Hourglass/Hourglass.ino
+++ Hourglass/Hourglass.ino
@@ -12,23 +12,20 @@ int mapMatrix1[8][8];
 int mapMatrix2[8][8];
 bool hilfsmatrix[8][8];
 
+bool kopf = true; //Wird anhand der Ausrichtung des Gyros bestimmt
+int setzenX = 0; //X Koordinate die beschrieben werden soll
+int setzenY = 0; //Y Koordinate die beschrieben werden soll
+int entfernenX = 0; //X Koordinate die entfernt werden soll
+int entfernenY = 0; //Y Koordinate die entfernt werden soll
+int state = 0; //Intervall für Sekunden pro Minute
+
+MD_MAX72XX matrix = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
 //Gyro
 #include <Wire.h>
 #include <MPU6050_light.h>
 
-MD_MAX72XX matrix = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-
 MPU6050 mpu(Wire);
-
-void setMapMatrix(int matrix){
-  
-}
-
-void getRowAndColumnFromID(int ID, int matrix) {
-  
-}
-
-
 
 void setup() {
   Serial.begin(9600);
@@ -38,6 +35,15 @@ void setup() {
   matrix.begin();
   matrix.control(MD_MAX72XX::INTENSITY, 0.2);
   matrix.clear();
+  
+  // Setzen der unteren 60 LEDs (weil 60sek/min)
+  for (int i = 8; i <= 15; i++) {    
+    matrix.setColumn(i, 0xff);
+  }
+  matrix.setPoint(7, 15, false);
+  matrix.setPoint(6, 15, false);
+  matrix.setPoint(7, 14, false);
+  matrix.setPoint(6, 14, false);
 
   //Gyro
   Wire.begin();
@@ -46,24 +52,8 @@ void setup() {
   delay(100);
   mpu.calcGyroOffsets();
   Serial.println("Done.");
- 
-
-  // setzen der unteren 60 LEDs (weil 60sek/min)
-  for (int i = 8; i <= 15; i++) {    
-    matrix.setColumn(i, 0xff);
-  }
-  matrix.setPoint(7, 15, false);
-  matrix.setPoint(6, 15, false);
-  matrix.setPoint(7, 14, false);
-  matrix.setPoint(6, 14, false);
+  
 }
-  bool kopf = true; //wird anhand der ausrichtung des Gyros bestimmt
-  int setzenX = 0; //X Koordinate die beschrieben werden soll
-  int setzenY = 0; //Y Koordinate die beschrieben werden soll
-  int entfernenX = 0; //X Koordinate die entfernt werden soll
-  int entfernenY = 0; //Y Koordinate die entfernt werden soll
-  int state = 0; //intervall für sekunden pro minute
-
      
 void loop() {
   mpu.update();
@@ -71,25 +61,24 @@ void loop() {
   Serial.print("X: " + String(mpu.getAngleX()));
   Serial.println("\tY: " + String(mpu.getAngleY()));
     
-  //Wert größer als die Möglichkeiten setzen 
+  //Wert größer als die Möglichkeiten setzen
   setzenX = 8;
   setzenY = 8;
 
   entfernenX = 8;
   entfernenY = 8;
 
-
-  if (state == 0){
+  if (state == 0) {
     swap();
-  } else if (state == 60){
+  } else if (state == 60) {
     kopf = false;
   }
 
-  if (state < 60 & kopf){
-    for(int i=1; i<=8; i++){
-      for(int j=9; j<=16; j++){
-        if(matrix.getPoint(i-1, j-1)){
-          if(i*(j-8) < entfernenX*entfernenY){
+  if ((state < 60) & kopf) {
+    for (int i=1; i<=8; i++) {
+      for (int j=9; j<=16; j++) {
+        if (matrix.getPoint(i-1, j-1)) {
+          if ((i*(j-8)) < (entfernenX*entfernenY)) {
             entfernenX = i;
             entfernenY = j-8;
           }
@@ -97,28 +86,29 @@ void loop() {
       }
     }
 
-    matrix.setPoint(entfernenX-1, entfernenY+7,false);
+    matrix.setPoint(entfernenX-1, entfernenY+7, false);
 
-
-    for(int i=1; i<=8; i++){
-      for(int j=1; j<=8; j++){
-        if(matrix.getPoint(i-1, j-1)==false){
-          if(i*j < setzenX*setzenY){
+    for (int i=1; i<=8; i++) {
+      for (int j=1; j<=8; j++) {
+        if (matrix.getPoint(i-1, j-1) == false) {
+          if ((i*j) < (setzenX*setzenY)) {
             setzenX = i;
             setzenY = j;
           }
         }
       }
     }
-    matrix.setPoint(setzenX-1, setzenY-1,true);
+	
+    matrix.setPoint(setzenX-1, setzenY-1, true);
     state++;
-  } else if (state > 0 & !kopf){
+	
+  } else if ((state > 0) & !kopf) {
    
-    //niedrigsten freien Punkt suchen
-    for(int i=1; i<=8; i++){
-      for(int j=1; j<=8; j++){
-        if(matrix.getPoint(i-1, j-1)){
-          if(i*j > entfernenX*entfernenY){
+    //Niedrigsten freien Punkt suchen
+    for (int i=1; i<=8; i++) {
+      for (int j=1; j<=8; j++) {
+        if (matrix.getPoint(i-1, j-1)) {
+          if ((i*j) > (entfernenX*entfernenY)) {
             entfernenX = i;
             entfernenY = j;
           }
@@ -127,10 +117,10 @@ void loop() {
     } 
     matrix.setPoint(entfernenX-1, entfernenY-1, false);
 
-    for(int i=1; i<=8; i++){
-      for(int j=9; j<=16; j++){
-        if(matrix.getPoint(i-1, j-1)==false){
-          if(i*(j-8) > setzenX*setzenY){
+    for (int i=1; i<=8; i++) {
+      for (int j=9; j<=16; j++) {
+        if (matrix.getPoint(i-1, j-1) == false) {
+          if ((i*(j-8)) > (setzenX*setzenY)) {
             setzenX = i;
             setzenY = j-8;
           }
@@ -159,38 +149,39 @@ void loop() {
 }
 
 
-void swap(){
+void swap() {
   
-  for(int i=0;i<8;i++){
-    for(int j=0;j<8;j++){
-      if(matrix.getPoint(i,j)){
-        hilfsmatrix[7-i][7-j]= true;
+  for (int i=0; i<8; i++) {
+    for (int j=0; j<8; j++) {
+      if (matrix.getPoint(i,j)) {
+        hilfsmatrix[7-i][7-j] = true;
       }
-      else{
-        hilfsmatrix[7-i][7-j]= false;
-      }
-    }
-  }
-  for(int i=0;i<8;i++){
-    for(int j=0;j<8;j++){
-      matrix.setPoint(i,j, hilfsmatrix[i][j]);
-    }
-  }
-
-  for(int i=0;i<8;i++){
-    for(int j=8;j<16;j++){
-      if(matrix.getPoint(i,j)){
-        hilfsmatrix[7-i][7-(j-8)]= true;
-      }
-      else{
-        hilfsmatrix[7-i][7-(j-8)]= false;
+      else {
+        hilfsmatrix[7-i][7-j] = false;
       }
     }
   }
-  for(int i=i;i<8;i++){
-    for(int j=0;j<8;j++){
-      matrix.setPoint(i,j+8, hilfsmatrix[i][j]);
+  
+  for (int i=0; i<8; i++) {
+    for (int j=0; j<8; j++) {
+      matrix.setPoint(i, j, hilfsmatrix[i][j]);
     }
   }
 
+  for (int i=0; i<8; i++) {
+    for (int j=8; j<16; j++) {
+      if (matrix.getPoint(i,j)) {
+        hilfsmatrix[7-i][7-(j-8)] = true;
+      }
+      else {
+        hilfsmatrix[7-i][7-(j-8)] = false;
+      }
+    }
+  }
+  
+  for (int i=i; i<8; i++) {
+    for (int j=0; j<8; j++) {
+      matrix.setPoint(i, j+8, hilfsmatrix[i][j]);
+    }
+  }
 }
